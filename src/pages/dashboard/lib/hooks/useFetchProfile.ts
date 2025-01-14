296a26e149c5d4cfb72f1156eca24e6054d8642b
MrQuackDuck <mrquackduckk@gmail.com>
2025-01-14 22:45:04 +0200

    chore: remove commented out code

--- src/pages/dashboard/lib/hooks/useFetchProfile.ts
+++ src/pages/dashboard/lib/hooks/useFetchProfile.ts
@@ -1,31 +1,8 @@
-// import { CurrencyService } from "@/entities/currency/api/CurrencyService";
-// import { useCurrencyStore } from "@/entities/currency/lib/hooks/useCurrencyStore";
-// import { errorToast } from "@/shared/lib/errorToast";
-// import { useAuthStore } from "@/shared/lib/hooks/useAuthStore";
-// import { useEffect } from "react";
-// import { useQuery } from "react-query";
-
 import { ProfileService } from "@/entities/user/api/ProfileService";
 import { useProfileStore } from "@/entities/user/lib/hooks/useProfileStore";
 import { useAuthStore } from "@/shared/lib/hooks/useAuthStore";
 import { useEffect } from "react";
 import { useQuery } from "react-query";
-
-// export const useFetchCurrencies = () => {
-//   const logOut = useAuthStore(state => state.setUnauthenticated);
-
-//   const setCurrencis = useCurrencyStore((state) => state.setCurrencies);
-//   const { data, isError, error } = useQuery("currencies", async () => {
-//     return (await CurrencyService.getCurrencyList()).data.data;
-//   });
-
-//   useEffect(() => {
-//     if ((error as any)?.status === 401) return logOut();
-//     if (isError) return errorToast("Failed to fetch currencies", "Please try again later");
-//     if (!data) return;
-//     setCurrencis(data.items);
-//   }, [data, isError, error, logOut, setCurrencis]);
-// }
 
 export const useFetchProfile = () => {
   const logOut = useAuthStore(state => state.setUnauthenticated);
